c78d146c956d10586807af266877fd97b837aec9
Harish M <harishmadamanchi95@gmail.com>
2021-09-04 08:12:14 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import Navbar from './Components/Navbar';
+import Head from './Components/Head';
+import CardSection from './Components/CardSection';
+import Footer from './Components/Footer';
 
 function App() {
+
+  const [data, setData] = useState(
+    [
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Fancy Product',
+          presentPrice : '$40.00 - $80.00',
+          sale : false,
+          cart : false
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Special Item',
+          oldPrice : '$20.00',
+          presentPrice : '$18.00',
+          sale : true,
+          rating : 5,
+          cart : false
+          
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Sale Item',
+          oldPrice : '$50.00',
+          presentPrice : '$25.00',
+          sale : true,
+          cart : false
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Popular Item',
+          presentPrice : '$40.00',
+          sale : false,
+          rating : 5,
+          cart : false
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Sale Item',
+          oldPrice : '$50.00',
+          presentPrice : '$25.00',
+          sale : true,
+          cart : false
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Fancy Product',
+          presentPrice : '$120.00 - $280.00',
+          sale : false,
+          cart : false
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Special Item',
+          oldPrice : '$20.00',
+          presentPrice : '$18.00',
+          sale : true,
+          cart : false
+      },
+      {
+          url: 'https://dummyimage.com/450x300/dee2e6/6c757d.jpg',
+          name : 'Popular Item',
+          presentPrice : '$40.00',
+          sale : false,
+          rating : 5,
+          cart : false
+      }
+  ]
+  )
+  const [cartCount, setCartCount] = useState(0);
+
+  const addToCart = (key) => {
+    
+    let datas = data;
+    datas[key].cart = true;
+    setData(datas);
+    // setData([...data,data[key].cart = true])
+    setCartCount(cartCount + 1);
+  }
+
+  const removeFromCart = (key) => {
+    
+    let datas = data;
+    datas[key].cart = false;
+    setData(datas);
+    // setData([...data,data[key].cart = false])
+    setCartCount(cartCount - 1);
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar cartCount = {cartCount}/>
+      <Head />
+      <CardSection data = {data} addToCart = {addToCart} removeFromCart = {removeFromCart}/>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Components/Card.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export default function Card({item, addItem, uniValue, removeCart}) {
+  return (
+    <div className="col mb-5">
+      <div className="card h-100">
+          {/* <!-- Sale badge--> */}
+          {item.sale ? (<div
+          className="badge bg-dark text-white position-absolute"
+          style={{top: "0.5rem", right: "0.5rem"}}
+        >
+          Sale
+        </div>): ''}
+        
+        {/* <!-- Product image--> */}
+        <img
+          className="card-img-top"
+          src= {item.url}
+          alt="..."
+        />
+        {/* <!-- Product details--> */}
+        <div className="card-body p-4">
+          <div className="text-center">
+            {/* <!-- Product name--> */}
+            <h5 className="fw-bolder">{item.name}</h5>
+            {/* <!-- Product reviews--> */}
+            {item.rating ? 
+            (<div className="d-flex justify-content-center small text-warning mb-2">
+            <div className="bi-star-fill"></div>
+            <div className="bi-star-fill"></div>
+            <div className="bi-star-fill"></div>
+            <div className="bi-star-fill"></div>
+            <div className="bi-star-fill"></div>
+          </div> ): ''}
+            
+            {/* <!-- Product price--> */}
+            {item.oldPrice ? (<span className="text-muted text-decoration-line-through">{item.oldPrice}</span>) : ''}
+            {item.presentPrice}
+          </div>
+        </div>
+        {/* <!-- Product actions--> */}
+        <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+          <div className="text-center">
+              {item.cart ? 
+                <a className="btn btn-danger mt-auto" href="#" onClick = {() => {removeCart(uniValue)}}>
+                    Remove
+                </a>
+                  :
+                <a className="btn btn-outline-dark mt-auto" href="#" onClick = {() => {addItem(uniValue)}}>
+                    Add to cart
+                </a>
+              }
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
